5e2e6bdcd6b8beb1144987462dce4538c2b70440
Supernats <nathanseither@gmail.com>
2014-01-15 01:16:55 -0800

    give up on fish, ack, and homebrew, using pathogen

--- install.rb
+++ install.rb
@@ -29,25 +29,38 @@ end
 Dir.chdir
 print "done\n"
 
-print "Installing homebrew..."
-Dir.chdir(PATH)
-hb = system("./homebrew_install.rb")
-print hb ? "done" : "failed"
+print "Installing pathogen.vim..."
+pathogen = system("mkdir -p ~/.vim/autoload ~/.vim/bundle; \ " +
+"curl -Sso ~/.vim/autoload/pathogen.vim \ " +
+    "https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim")
+print pathogen ? "done" : "failed"
 print "\n"
 
-print "Installing ack..."
-ack = system("brew install ack")
-print ack ? "done" : "failed"
+print "Installing NERDTree..."
+nerd = system("cd ~/.vim/bundle &" +
+"git clone https://github.com/scrooloose/nerdtree.git")
+print nerd ? "done" : "failed"
 print "\n"
 
-print "Installing fish..."
-fish = system("brew install fish")
-print fish ? "done" : "failed"
+print "Installing ctrlp.vim..."
+ctrlp = system("git clone https://github.com/kien/ctrlp.vim ~/.vim/bundles/ctrlp.vim")
+print ctrlp ? "done" : "failed"
 print "\n"
 
-print "Installing Janus..."
-janus = system("curl -Lo- https://bit.ly/janus-bootstrap | bash")
-print janus ? "done" : "failed"
+print "Installing Rainbow Parentheses..."
+rain = system("git clone https://github.com/kien/rainbow_parentheses.vim ~/.vim/bundles/rainbow_parentheses.vim")
+print rain ? "done" : "failed"
+print "\n"
+
+print "Installing Fugitive..."
+fugitive = system("git clone https://github.com/tpope/vim-fugitive ~/.vim/bundles/vim-fugitive")
+print fugitive ? "done" : "failed"
+print "\n"
+
+print "Installing Indent Guides..."
+indent = system("git clone https://github.com/nathanaelkane/vim-indent-guides ~/.vim/bundles/vim-indent-guides
+")
+print indent ? "done" : "failed"
 print "\n"
 
 print "Applying your .vimrc..."
@@ -55,7 +68,7 @@ Dir.chdir(PATH)
 Dir.chdir("master_settings")
 vimrc_master = File.read(".vimrc")
 Dir.chdir
-File.open(".vimrc.after", "w") do |f|
+File.open(".vimrc", "w") do |f|
   f.puts vimrc_master
 end
 print "done\n"
@@ -63,10 +76,11 @@ print "done\n"
 print "Applying your .bashrc..."
 Dir.chdir(PATH)
 Dir.chdir("master_settings")
-bashrc_master = File.read(".vimrc")
+bashrc_master = File.read(".bashrc")
 Dir.chdir
 File.open(".bashrc", "w") do |f|
   f.puts bashrc_master
 end
 print "done\n"
 
+puts "ENJOY YOUR AWESOME TERMINAL"
---
+++ master_settings/.bashrc
@@ -0,0 +1,33 @@
+PROMPT_COLOR='\e[00m\e[38;05;166m'
+export PS1='\['$PROMPT_COLOR'\]\w\$\[\e[0m\] '
+export EDITOR=emacs
+
+MILLION=1000000
+export HISTFILESIZE=$MILLION
+export HISTSIZE=$MILLION
+
+# update bash history after each command
+export PROMPT_COMMAND="history -a"
+
+export MPORTS_DIR=/opt/local/bin:/opt/local/sbin
+# prefer macports stuff
+PATH=$MPORTS_DIR:$PATH
+#export EMACS='/opt/local/bin/emacs'
+export LS='/opt/local/bin/gls'
+
+alias e=emacs
+alias ls='$LS --color=auto'
+alias l='ls'
+
+# I never remember to prefer pry
+alias irb='pry'
+
+source .dotfiles/git-completion.bash
+
+PATH=$PATH:$HOME/.rvm/bin
+source ~/.rvm/scripts/rvm
+source $rvm_path/scripts/completion
+
+PATH="/usr/local/mysql/bin:$PATH"
+PATH="/Applications/Postgres.app/Contents/MacOS/bin:$PATH"
+
