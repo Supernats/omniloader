70988ad391654f2740cc685553b254a218f1626e
Supernats <nathanseither@gmail.com>
2014-01-16 23:45:36 -0800

    separate vimrails and vimbundler

--- install.rb
+++ install.rb
@@ -55,10 +55,15 @@ puts indent ? "done" : "failed"
 
 puts "Installing rails.vim..."
 vimrails = system(
-  "git clone git://github.com/tpope/vim-rails.git  ~/.vim/bundle/vim-rails" &&
-  "git clone git://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundler"
+  "git clone git://github.com/tpope/vim-rails.git  ~/.vim/bundle/vim-rails"
 )
 puts vimrails ? "done" : "failed"
+
+puts "Installing vim bundler..."
+vimbundler = system(
+  "git clone git://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundler"
+)
+puts vimbundler ? "done" : "failed"
 
 puts "Installing commentary.vim..."
 commentary = system(
