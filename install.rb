574e87beeec9d506475ba25468f1720fd6297c1f
App Academy Student <student@appacademy.io>
2014-01-26 19:28:34 -0800

    Finish down script

--- install.rb
+++ install.rb
@@ -8,6 +8,7 @@ Dir.chdir
 print "Making setup directory..."
 Dir.mkdir("omniloader_setup") unless Dir.exists?("omniloader_setup")
 print "done\n"
+puts
 
 print "Copying .vimrc..."
 vimrc_copy = File.exists?(".vimrc") ? File.read(".vimrc") : ""
@@ -17,6 +18,7 @@ File.open("vimrc_copy", "w") do |f|
 end
 Dir.chdir
 print "done\n"
+puts
 
 print "Copying .bashrc..."
 bashrc_copy = File.exists?(".bashrc") ? File.read(".bashrc") : ""
@@ -26,92 +28,109 @@ File.open("bashrc_copy", "w") do |f|
 end
 Dir.chdir
 print "done\n"
+puts
 
 puts "Installing pathogen.vim..."
 pathogen = system("mkdir -p ~/.vim/autoload ~/.vim/bundle
 curl -so ~/.vim/autoload/pathogen.vim \
     https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim")
 puts pathogen ? "done" : "failed"
+puts
 
 puts "Installing NERDTree..."
 nerd = system("git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree.vim")
 puts nerd ? "done" : "failed"
+puts
 
 puts "Installing ctrlp.vim..."
 ctrlp = system("git clone https://github.com/kien/ctrlp.vim ~/.vim/bundle/ctrlp.vim")
 puts ctrlp ? "done" : "failed"
+puts
 
 puts "Installing Rainbow Parentheses..."
 rain = system("git clone https://github.com/kien/rainbow_parentheses.vim ~/.vim/bundle/rainbow_parentheses.vim")
 puts rain ? "done" : "failed"
+puts
 
 puts "Installing Fugitive..."
 fugitive = system("git clone https://github.com/tpope/vim-fugitive ~/.vim/bundle/vim-fugitive")
 puts fugitive ? "done" : "failed"
+puts
 
 puts "Installing Indent Guides..."
 indent = system("git clone https://github.com/nathanaelkane/vim-indent-guides ~/.vim/bundle/vim-indent-guides")
 puts indent ? "done" : "failed"
+puts
 
 puts "Installing rails.vim..."
 vimrails = system(
   "git clone git://github.com/tpope/vim-rails.git  ~/.vim/bundle/vim-rails"
 )
 puts vimrails ? "done" : "failed"
+puts
 
 puts "Installing vim bundler..."
 vimbundler = system(
   "git clone git://github.com/tpope/vim-bundler.git ~/.vim/bundle/vim-bundler"
 )
 puts vimbundler ? "done" : "failed"
+puts
 
 puts "Installing commentary.vim..."
 commentary = system(
   "git clone git://github.com/tpope/vim-commentary.git ~/.vim/bundle/vim-commentary"
 )
 puts commentary ? "done" : "failed"
+puts
 
 puts "Installing Vim Backbone..."
 backbone = system(
   "git clone git://github.com/mklabs/vim-backbone.git ~/.vim/bundle/vim-backbone"
 )
 puts backbone ? "done" : "failed"
+puts
 
 puts "Installing Vim-Javascript..."
 javascript = system(
   "git clone https://github.com/pangloss/vim-javascript.git ~/.vim/bundle/vim-javascript"
 )
 puts javascript ? "done" : "failed"
+puts
 
 puts "Installing rename.vim..."
 rename = system(
   "git clone https://github.com/danro/rename.vim.git ~/.vim/bundle/vim-rename"
 )
 puts rename ? "done" : "failed"
+puts
 
 puts "Installing TLib.vim..."
 tlib = system(
   "git clone https://github.com/tomtom/tlib_vim.git ~/.vim/bundle/tlib"
 )
 puts tlib ? "done" : "failed"
+puts
 
 puts "Installing MW Utils..."
 mwutils = system(
   "git clone https://github.com/MarcWeber/vim-addon-mw-utils.git ~/.vim/bundle/mw_utils"
 )
 puts mwutils ? "done" : "failed"
+puts
 
 puts "Installing Vim Snippets..."
 snippets = system(
   "git clone https://github.com/honza/vim-snippets.git ~/.vim/bundle/snippets"
 )
 puts snippets ? "done" : "failed"
+puts
 
 puts "Installing Snipmate.vim..."
 snipmate = system(
   "git clone https://github.com/garbas/vim-snipmate.git ~/.vim/bundle/snipmate"
 )
 puts snipmate ? "done" : "failed"
+puts
 
 print "Applying your .vimrc..."
 vimrc_master = `curl https://raw.github.com/Supernats/omniloader/master/master_settings/.vimrc`
@@ -121,6 +140,7 @@ File.open("vimrc", "w") do |f|
 end
 `mv vimrc .vimrc`
 print "done\n"
+puts
 
 print "Applying your .bashrc..."
 bashrc_master = `curl https://raw.github.com/Supernats/omniloader/master/master_settings/.bashrc`
@@ -129,6 +149,7 @@ File.open(".bashrc", "w") do |f|
   f.puts bashrc_master
 end
 print "done\n"
+puts
 
 puts "-------------------------------------------------------------------------"
 puts "                              End OMNILOADER                             "
--- uninstall.rb
+++ uninstall.rb
@@ -10,7 +10,40 @@ Dir.chdir("omniloader_setup")
 print "Copying backup .vimrc..."
 vimrc_copy = File.read("vimrc_copy")
 print "done"
+puts
 
 print "Copying backup .bashrc..."
 bashrc_copy = File.read("bashrc_copy")
 print "done"
+puts 
+
+Dir.chdir
+print "Deleting .vim directrory..."
+vim_dir = system("rm -rf .vim")
+print vim_dir ? "done" : "failed"
+puts 
+
+print "Reverting to old .vimrc..."
+File.open("vimrc", "w") do |f|
+  f.puts vimrc_copy
+end
+`mv vimrc .vimrc`
+print "done\n"
+puts
+
+print "Reverting to old .bashrc..."
+File.open(".bashrc", "w") do |f|
+  f.puts bashrc_copy
+end
+print "done\n"
+puts
+
+prints "Removing setupd directory..."
+remove = system("rm -rf omniloader_setup")
+print remove ? "done" : "failed"
+puts 
+
+puts "-------------------------------------------------------------------------"
+puts "                              End OMNILOADER                             "
+puts "-------------------------------------------------------------------------"
+puts "ENJOY BEING VANILLA AND BORING"
