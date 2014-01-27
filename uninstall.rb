de040ba30b3a8f24c676be40fde02d893460f624
App Academy Student <student@appacademy.io>
2014-01-26 18:54:56 -0800

    Begin uninstall

--- uninstall.rb
+++ uninstall.rb
@@ -1,1 +1,16 @@
+#!/usr/bin/env ruby
+#
+puts "-------------------------------------------------------------------------"
+puts "                             Begin OMNILOADER                            "
+puts "-------------------------------------------------------------------------"
 
+Dir.chdir
+Dir.chdir("omniloader_setup")
+
+print "Copying backup .vimrc..."
+vimrc_copy = File.read("vimrc_copy")
+print "done"
+
+print "Copying backup .bashrc..."
+bashrc_copy = File.read("bashrc_copy")
+print "done"
